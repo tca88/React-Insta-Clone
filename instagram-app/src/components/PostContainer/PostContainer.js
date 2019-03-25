64ea812a00185ed4717fdc97bff87bf5bd1b75df
tca88 <trishnaamin@gmail.com>
2019-03-25 19:08:45 -0400

    Add proptypes check into PostContainer.js.

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -3,6 +3,7 @@ import CommentSection from '../CommentSection/CommentSection.js';
 import like from '../img/like.png';
 import comment from '../img/comment.png';
 import './PostContainer.css';
+import PropTypes from "prop-types";
 
 const PostContainer = props => {
     
@@ -35,6 +36,25 @@ const PostContainer = props => {
             </div>
         </div>
     )
+}
+
+PostContainer.propTypes = {
+    instagramData: PropTypes.arrayOf(
+        PropTypes.shape({
+        username: PropTypes.string,
+        thumbnailUrl: PropTypes.string,
+        imageUrl: PropTypes.string,
+        likes: PropTypes.number,
+        timestamp: PropTypes.string,
+        comments: PropTypes.arrayOf(
+            PropTypes.shape({
+            username: PropTypes.string,
+            text: PropTypes.string,
+            name: PropTypes.string,
+            })
+        )
+        })
+        )
 }
 
 export default PostContainer;
