0347ec5acdf5472ad20d1d27754bfc2e9b423be6
tca88 <trishnaamin@gmail.com>
2019-03-28 16:00:07 -0400

    Add global styles to App.js file.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,8 +2,31 @@ import React, { Component } from "react";
 import PostsPage from "./components/PostContainer/PostsPage.js";
 import withAuthenticate from "./components/authentication/withAuthenticate";
 import Login from "./components/Login/Login";
-import styled, { css } from "styled-components";
+import styled, { css, createGlobalStyle } from "styled-components";
 import { Reset } from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+* html {
+  box-sizing: border-box;
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-size: 62.5%;
+  font-family: sans-serif;
+  background-image: linear-gradient(
+    to right,
+    #5853c9,
+    #c33082,
+    #f56f32,
+    #fed371
+  );
+  height: 100%;
+}
+
+  `;
 
 const ComponentFromWithAuthenticate = withAuthenticate(PostsPage)(Login);
 
@@ -22,6 +45,7 @@ class App extends Component {
     return (
       <div>
         <Reset />
+        <GlobalStyles />
         <ComponentFromWithAuthenticate loggedIn={this.state.loggedIn} />
       </div>
     );
